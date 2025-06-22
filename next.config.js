05b0225d8539bfc885b7c3bfe1a2710b207175c3
mousetouch5 <joaoroumilvergara@gmail.com>
2025-06-23 00:42:16 +0800

    fix some config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/asana-clone",
+  assetPrefix: "/asana-clone/",
+};
 
-    
-
-
-
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
